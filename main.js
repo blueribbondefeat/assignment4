e3aa0dd3fe3ff607b7c914f553de1cf6ba22e5f9
Amanda Hagg <haggamanda@gmail.com>
2015-02-10 23:20:44 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+alert("Hello World");
+alert(4+4+4);
+console.log("This is console output from an internal script.");
+/*
+4. Write an external script that alerts "It is great to meet you.
+	- Alerts "This is happening inside an external script" to the user.
+	- Alerts the product of 3 numbers to the user.
+	- Print "This is console output from an internal script."
+	- Changes the value of the <h1 id="name"> element to your name.
+	- Changes the value of the <p id="desc"> element to a paragraph element to the text of your choice.
+*/
